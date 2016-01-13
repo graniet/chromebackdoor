dc176470689cf220ffbdf341323cf0484dd3fe98
tristan.granier <tristan.granier@epitech.eu>
2016-01-13 19:56:59 +0100

    v2

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_GET['info']) && isset($_GET['url']) && $_GET['info'] != '' && $_GET['url'])
+if(isset($_GET['info']) && isset($_GET['url']) && $_GET['info'] != '' && $_GET['url'] != '')
 {
     
 }

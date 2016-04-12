9db8ebac5b9e3972f0446cedde436f60a1ec7cdd
Tristan Granier <graniet75@gmail.com>
2016-04-12 09:29:47 +0200

    facebook spy v1.1

--- facebookmessage.js
+++ facebookmessage.js
@@ -16,6 +16,9 @@ function GetMessage(){
 
 function show_convers(ids){
         var old_span = "";
+        var message_array = [];
+        var message_owner = [];
+
         $('.conversationActuel').html('<div class="nameTitle">Conversation title</div>');
         $('.oldmessage .conversation').each(function(index, value){
                 var title  = $('.titlebarTextWrapper')[ids].innerText;
@@ -27,12 +30,19 @@ function show_convers(ids){
                     var span = bullet[x].querySelectorAll('span');
                     if(span.length > 1){
                         for(var y = 0;y < span.length; y++){
-                            if(avatar != null && span[y].innerText != '' && span[y].innerText != old_span){
+                            if(avatar != null && span[y].innerText != '' && span[y].innerText != old_span && message_array.indexOf(span[y].innerText) < 0){
                                 if(old_span != span[y].innerText){
+                                    message_array.push(span[y].innerText);
                                     var div = '<div class="message"><div class="message-left"><img src="'+avatar.src+'" /></div> <div class="message-right">'+span[y].innerText+'</div></div><div class="clear"></div>';
                                     $('.conversationActuel').append(div);
                                     old_span = span[y].innerText;
                                 }
+                            }
+                            else if(avatar == null && span[y].innerText != '' && span[y].innerText != old_span && message_owner.indexOf(span[y].innerText) < 0){
+                                    message_owner.push(span[y].innerText);
+                                    var div = '<div class="message"><div class="message-rightOwner">'+span[y].innerText+'</div><div class="message-leftOwner"><img src="http://cdn.ipetitions.com/rev/176/assets/v3/img/default-avatar.png" /></div></div><div class="clear"></div>';
+                                    $('.conversationActuel').append(div);
+                                    old_span = span[y].innerText;                                
                             }
                         }
                     }
